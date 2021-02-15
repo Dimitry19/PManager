c249f39dfed4f676c185ed285000dedbc0480442
Dimitri Sime <unkutnation@gmail.com>
2021-02-15 14:34:59 +0000

    Debut de l'implementation de la gestion des mails avec SendGrid API

--- src/main/java/cm/packagemanager/pmanager/common/mail/MailSender.java
+++ src/main/java/cm/packagemanager/pmanager/common/mail/MailSender.java
@@ -56,6 +56,11 @@ public class MailSender {
 
 	@Value("${mail.email.from}")
 	private String fromName;
+	
+	
+	@Value("${sendgrid.api.key}")
+	private String SENDGRID_API_KEY;
+
 
 	Properties properties;
 
@@ -101,7 +106,7 @@ public class MailSender {
 			MimeMessage message = new MimeMessage(mailSession);
 			if(from==null){
 				from="noreply@travel.com";
-				username ="no reply";
+				username ="No reply";
 			}
 
 			message.setFrom(new InternetAddress(from, username));
@@ -138,8 +143,7 @@ public class MailSender {
 					emailTemplateString = emailTemplateString.replaceAll("(?i)" + "%" + entry.getKey() + "%", value);
 				}
 			}
-			catch (Exception e)
-			{
+			catch (Exception e){
 			}
 
 			message.setContent(emailTemplateString, "text/html");
@@ -249,22 +253,265 @@ public class MailSender {
 		return variableLabel;
 	}
 
-	class SMTPAuthenticator extends javax.mail.Authenticator
-	{
+	class SMTPAuthenticator extends javax.mail.Authenticator{
 		String username;
 		String password;
 
-		public SMTPAuthenticator(String username, String password)
-		{
+		public SMTPAuthenticator(String username, String password){
 			this.username = username;
 			this.password = password;
 		}
 
-		public PasswordAuthentication getPasswordAuthentication()
-		{
+		public PasswordAuthentication getPasswordAuthentication(){
 			return new PasswordAuthentication(username, password);
 		}
 	}
+	
+	
+	 private  void send(final Mail mail) throws IOException {
+	    final SendGrid sg = new SendGrid(SENDGRID_API_KEY);
+	    sg.addRequestHeader("X-Mock", "true");
+
+	    final Request request = new Request();
+	    request.setMethod(Method.POST);
+	    request.setEndpoint("mail/send");
+	    request.setBody(mail.build());
+
+	    final Response response = sg.api(request);
+	    System.out.println(response.getStatusCode());
+	    System.out.println(response.getBody());
+	    System.out.println(response.getHeaders());
+	  }
+	
+	public static Mail buildMailToSend(String templateName, String messageSubject, Map<String, String> variableLabel, List<String> emailSendTo, List<String> emailSendCC,List<String> emailSendBCC,String from,String username) {
+	    Mail mail = new Mail();
+		
+		EmailSendAddresses=formatEmails(emailSendTo);
+		EmailSendAddressesCC=formatEmails(emailSendCC);
+		EmailSendAddressesBCC=formatEmails(emailSendBCC);
+
+
+		String[] EmailSendAddressesArray = EmailSendAddresses.split(SEPARATOR);
+		String[] EmailSendAddressesCCArray = StringUtils.isNotEmpty(EmailSendAddressesCC)?EmailSendAddressesCC.split(SEPARATOR):null;
+		String[] EmailSendAddressesBCCArray = StringUtils.isNotEmpty(EmailSendAddressesBCC)?EmailSendAddressesBCC.split(SEPARATOR):null;
+	
+		Resource resource = new ClassPathResource("/templates/"+templateName);
+
+		InputStream emailTemplateStream = resource.getInputStream();
+
+		String emailTemplateString =Utility.convertStreamToString(emailTemplateStream);
+		
+		if(from==null){
+			from="noreply@travel.com";
+			username ="No reply";
+		}
+
+	    Email fromEmail = new Email();
+	    fromEmail.setName(username);
+	    fromEmail.setEmail(from);
+	    mail.setFrom(fromEmail);
+	    mail.setSubject(messageSubject);
+
+		Personalization personalization = new Personalization();
+		for (int i = 0; i < EmailSendAddressesArray.length; i++){
+	    	Email to = new Email();
+			to.setName("Example User");
+	        to.setEmail(EmailSendAddressesArray[i]);
+			personalization.addTo(to)
+			
+		}
+		
+		for (int i = 0; i < EmailSendAddressesCCArray.length; i++){
+			Email cc = new Email();
+			cc.setName("Example User");
+	        cc.setEmail(EmailSendAddressesCCArray[i]);
+			personalization.addCc(cc);
+			
+		}
+		
+		for (int i = 0; i < EmailSendAddressesBCCArray.length; i++){
+			Email bcc = new Email();
+			bcc.setName("Example User");
+	        bcc.setEmail(EmailSendAddressesBCCArray[i]);
+			personalization.addBcc(bcc);
+			
+		}
+	   /*Personalization personalization = new Personalization();
+	    Email to = new Email();
+	    to.setName("Example User");
+	    to.setEmail("test1@example.com");
+	    personalization.addTo(to);
+	    to.setName("Example User");
+	    to.setEmail("test2@example.com");
+	    personalization.addTo(to);
+	    Email cc = new Email();
+	    cc.setName("Example User");
+	    cc.setEmail("test3@example.com");
+	    personalization.addCc(cc);
+	    cc.setName("Example User");
+	    cc.setEmail("test4@example.com");
+	    personalization.addCc(cc);
+	    Email bcc = new Email();
+	    bcc.setName("Example User");
+	    bcc.setEmail("test5@example.com");
+	    personalization.addBcc(bcc);
+	    bcc.setName("Example User");
+	    bcc.setEmail("test6@example.com");
+	    personalization.addBcc(bcc);*/
+	    personalization.setSubject("Hello World from the Personalized Twilio SendGrid Java Library");
+	    personalization.addHeader("X-Test", "test");
+	    personalization.addHeader("X-Mock", "true");
+	    personalization.addSubstitution("%name%", "Example User");
+	    personalization.addSubstitution("%city%", "Riverside");
+	    personalization.addCustomArg("user_id", "343");
+	    personalization.addCustomArg("type", "marketing");
+	    personalization.setSendAt(1443636843);
+	    mail.addPersonalization(personalization);
+
+	    /*Personalization personalization2 = new Personalization();
+	    Email to2 = new Email();
+	    to2.setName("Example User");
+	    to2.setEmail("test1@example.com");
+	    personalization2.addTo(to2);
+	    to2.setName("Example User");
+	    to2.setEmail("test2@example.com");
+	    personalization2.addTo(to2);
+	    Email cc2 = new Email();
+	    cc2.setName("Example User");
+	    cc2.setEmail("test3@example.com");
+	    personalization2.addCc(cc2);
+	    cc2.setName("Example User");
+	    cc2.setEmail("test4@example.com");
+	    personalization2.addCc(cc2);
+	    Email bcc2 = new Email();
+	    bcc2.setName("Example User");
+	    bcc2.setEmail("test5@example.com");
+	    personalization2.addBcc(bcc2);
+	    bcc2.setName("Example User");
+	    bcc2.setEmail("test6@example.com");
+	    personalization2.addBcc(bcc2);
+	    personalization2.setSubject("Hello World from the Personalized Twilio SendGrid Java Library");
+	    personalization2.addHeader("X-Test", "test");
+	    personalization2.addHeader("X-Mock", "true");
+	    personalization2.addSubstitution("%name%", "Example User");
+	    personalization2.addSubstitution("%city%", "Denver");
+	    personalization2.addCustomArg("user_id", "343");
+	    personalization2.addCustomArg("type", "marketing");
+	    personalization2.setSendAt(1443636843);
+	    mail.addPersonalization(personalization2);*/
+
+	    Content content = new Content();
+	    content.setType("text/plain");
+	    content.setValue("some text here");
+	    mail.addContent(content);
+	    content.setType("text/html");
+	    content.setValue("<html><body>some text here</body></html>");
+	    mail.addContent(content);
+
+	    Attachments attachments = new Attachments();
+	    attachments.setContent("TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdC4gQ3JhcyBwdW12");
+	    attachments.setType("application/pdf");
+	    attachments.setFilename("balance_001.pdf");
+	    attachments.setDisposition("attachment");
+	    attachments.setContentId("Balance Sheet");
+	    mail.addAttachments(attachments);
+
+	    Attachments attachments2 = new Attachments();
+	    attachments2.setContent("BwdW");
+	    attachments2.setType("image/png");
+	    attachments2.setFilename("banner.png");
+	    attachments2.setDisposition("inline");
+	    attachments2.setContentId("Banner");
+	    mail.addAttachments(attachments2);
+
+	    /*mail.setTemplateId("13b8f94f-bcae-4ec6-b752-70d6cb59f932");
+
+	    mail.addSection("%section1%", "Substitution Text for Section 1");
+	    mail.addSection("%section2%", "Substitution Text for Section 2");
+
+	    mail.addHeader("X-Test1", "1");
+	    mail.addHeader("X-Test2", "2");
+
+	    mail.addCategory("May");
+	    mail.addCategory("2016");
+
+	    mail.addCustomArg("campaign", "welcome");
+	    mail.addCustomArg("weekday", "morning");
+
+	    mail.setSendAt(1443636842);
+
+	    ASM asm = new ASM();
+	    asm.setGroupId(99);
+	    asm.setGroupsToDisplay(new int[]{4, 5, 6, 7, 8});
+	    mail.setASM(asm);
+
+	    // This must be a valid [batch ID](https://sendgrid.com/docs/API_Reference/SMTP_API/scheduling_parameters.html) to work
+	    // mail.setBatchId("sendgrid_batch_id");
+
+	    mail.setIpPoolId("23");
+
+	    MailSettings mailSettings = new MailSettings();
+	    BccSettings bccSettings = new BccSettings();
+	    bccSettings.setEnable(true);
+	    bccSettings.setEmail("test@example.com");
+	    mailSettings.setBccSettings(bccSettings);
+	    Setting sandBoxMode = new Setting();
+	    sandBoxMode.setEnable(true);
+	    mailSettings.setSandboxMode(sandBoxMode);
+	    Setting bypassListManagement = new Setting();
+	    bypassListManagement.setEnable(true);
+	    mailSettings.setBypassListManagement(bypassListManagement);
+	    FooterSetting footerSetting = new FooterSetting();
+	    footerSetting.setEnable(true);
+	    footerSetting.setText("Footer Text");
+	    footerSetting.setHtml("<html><body>Footer Text</body></html>");
+	    mailSettings.setFooterSetting(footerSetting);
+	    SpamCheckSetting spamCheckSetting = new SpamCheckSetting();
+	    spamCheckSetting.setEnable(true);
+	    spamCheckSetting.setSpamThreshold(1);
+	    spamCheckSetting.setPostToUrl("https://spamcatcher.sendgrid.com");
+	    mailSettings.setSpamCheckSetting(spamCheckSetting);
+	    mail.setMailSettings(mailSettings);*/
+
+
+
+	    Email replyTo = new Email();
+	    replyTo.setName("Example User");
+	    replyTo.setEmail("test@example.com");
+	    mail.setReplyTo(replyTo);
+
+	    return mail;
+	  }
+	
+	private void testSend(){
+		
+		    Email from = new Email("packagemanager2020@gmail.com");
+		    String subject = "Sending with Twilio SendGrid is Fun";
+		    Email to = new Email("dimipasc@yahoo.fr");
+		    Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+		    Mail mail = new Mail(from, subject, to, content);
+
+		    SendGrid sg = new SendGrid("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");
+		    Request request = new Request();
+		    try {
+		      request.setMethod(Method.POST);
+		      request.setEndpoint("mail/send");
+		      request.setBody(mail.build());
+		      Response response = sg.api(request);
+		      System.out.println(response.getStatusCode());
+		      System.out.println(response.getBody());
+		      System.out.println(response.getHeaders());
+		      System.out.println(response.getBody().isEmpty());
+		    } catch (IOException ex) {
+		      throw ex;
+		    }
+		  
+	}
+	
+	
+	
+	
+	
 
 
 }
