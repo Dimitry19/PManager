f616e5266bb285ced650a79b376bae3b04e47ae9
Dimitri Sime <unkutnation@gmail.com>
2021-02-15 16:56:11 +0000

    Ajout de la description

--- src/main/java/cm/packagemanager/pmanager/common/enums/TransportEnum.java
+++ src/main/java/cm/packagemanager/pmanager/common/enums/TransportEnum.java
@@ -1,7 +1,31 @@
 package cm.packagemanager.pmanager.common.enums;
 
 public enum TransportEnum {
-	PLANE,
-	AUTO,
-	NAVE
+	PLANE("Avion"),
+	AUTO("Voiture"),
+	NAVE("Bateau")
+	
+	private String value;
+
+	private TransportEnum(String value) {
+		this.value = value;
+	}
+
+	public String toValue(){
+		return value;
+	}
+
+	public static TransportEnum fromValue(String value){
+		if(StringUtils.isEmpty(value)){
+			return null;
+		}
+
+		for (TransportEnum e : values()) {
+			if(e.value.equals(value)){
+				return e;
+			}
+		}
+
+		throw new RuntimeException("Valore " + value + " non valido");
+	}
 }
