c26454a78fc080e1354c4e6e9a18397fd4ba7155
Dimitry19 <orlynn1anais>
2021-04-13 01:00:08 +0200

    Upload image

--- src/main/java/cm/packagemanager/pmanager/ws/controller/rest/image/ImageController.java
+++ src/main/java/cm/packagemanager/pmanager/ws/controller/rest/image/ImageController.java
@@ -88,7 +88,7 @@ public class ImageController extends CommonController {
 	public ResponseEntity.BodyBuilder uploadImage(HttpServletRequest request, HttpServletResponse response,
 	                                                  @RequestParam("id") @Valid Long id,
 	                                                  @RequestParam("type") @Valid UploadImageType type,
-	                                                  @RequestParam("imageFile") MultipartFile file
+	                                                  @RequestParam("file") MultipartFile file
 	                                                  ) throws Exception {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		logger.info(" upload user image request in");
