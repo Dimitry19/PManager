39a79ffc46576122f445d4ab0fa5cc61cf56d31e
ludovic237 <cressonludoic@gmil.com>
2021-11-28 17:16:38 +0100

    integration Angular

--- src/main/java/cm/packagemanager/pmanager/ws/controller/DefaultController.java
+++ src/main/java/cm/packagemanager/pmanager/ws/controller/DefaultController.java
@@ -11,20 +11,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-//@Controller
+@Controller
 public class DefaultController {
 	private static final Logger logger = LoggerFactory.getLogger(DefaultController.class);
 
-   /// @GetMapping(value = "/")
+   // @GetMapping(value = "/")
 	public ResponseEntity<String> ping() {
 		logger.info("Démarrage des services OK .....");
 		return new ResponseEntity<String>("Réponse du serveur: "+HttpStatus.OK.name(), HttpStatus.OK);
 	}
 
-	//@RequestMapping("/index")
+	@RequestMapping("/index")
 	public String index() {
 
-		return "index";
+		return "/dist/index";
 	}
 
 }
