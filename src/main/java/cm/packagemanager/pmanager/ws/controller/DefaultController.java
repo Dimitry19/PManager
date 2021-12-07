adcbb16c8b2639bdaf27471d5c61bec416bef976
ludovic237 <cressonludoic@gmil.com>
2021-12-07 13:09:42 +0100

    test 1

--- src/main/java/cm/packagemanager/pmanager/ws/controller/DefaultController.java
+++ src/main/java/cm/packagemanager/pmanager/ws/controller/DefaultController.java
@@ -9,22 +9,22 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletResponse;
 
 
 @Controller
 public class DefaultController {
 	private static final Logger logger = LoggerFactory.getLogger(DefaultController.class);
 
-   // @GetMapping(value = "/")
-	public ResponseEntity<String> ping() {
+   @RequestMapping (value = "/", method = RequestMethod.GET)
+	public ResponseEntity<String> ping(HttpServletResponse response) {
 		logger.info("Démarrage des services OK .....");
+		response.setContentType("text/html");
 		return new ResponseEntity<String>("Réponse du serveur: "+HttpStatus.OK.name(), HttpStatus.OK);
+	   //return "public/index";
 	}
 
-	@RequestMapping("/index")
-	public String index() {
-
-		return "/dist/index";
-	}
 
 }
