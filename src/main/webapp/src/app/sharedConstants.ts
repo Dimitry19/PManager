08419e2a8e2721a109b1ac35718b1e8ba3e6f7d5
ludovic237 <cressonludoic@gmil.com>
2022-02-12 08:38:18 +0100

    code source...

---
+++ src/main/webapp/src/app/sharedConstants.ts
@@ -0,0 +1,66 @@
+export class SharedConstants {
+
+  public static Categories = [
+     {
+       value: "AUTRES",
+       description: "Autres"
+     },
+     {
+       value: "ELECTRONIQUES",
+       description: "Electroniques"
+     },
+     {
+       value: "VETEMENTS",
+       description: "Vetements"
+     },
+     {
+       value: "BIJOUX",
+       description: "Bijoux"
+     },
+     {
+       value: "DOCUMENTS",
+       description: "Documents"
+     }
+  ];
+  public static Transports = [
+     {
+       value: "",
+       description: "Indifférent"
+     },
+     {
+       value: "AUTO",
+       description: "Routier"
+     },
+     {
+       value: "PLANE",
+       description: "Aerien"
+     },
+     {
+       value: "NAVE",
+       description: "Maritime"
+     }
+  ];
+  public static Villes = [
+    {
+      id: 1,
+      name: 'MILAN'
+    },
+    {
+      id: 2,
+      name: 'BOLOGNE'
+    },
+    {
+      id: 3,
+      name: 'DOUALA'
+    },
+    {
+      id: 4,
+      name: 'YAOUNDE'
+    },
+    {
+      id: 5,
+      name: 'PARIS'
+    }
+ ];
+
+}
