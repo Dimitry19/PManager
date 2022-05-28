b708caf029154ba54ff981cd1498ed6e750b68a7
Dimitri Sime <unkutnation@gmailcom>
2022-05-28 13:05:38 +0200

    Correction estimate value sur la reservation

--- src/main/java/cm/travelpost/tp/announce/ent/dao/ReservationDAOImpl.java
+++ src/main/java/cm/travelpost/tp/announce/ent/dao/ReservationDAOImpl.java
@@ -168,6 +168,7 @@ public class ReservationDAOImpl extends Generic implements ReservationDAO<Reserv
         if (StringUtils.isNotEmpty(reservationDTO.getDescription())) {
             reservation.setDescription(reservationDTO.getDescription());
         }
+        reservation.setEstimateValue(reservationDTO.getEstimateValue());
         update(reservation);
 
         BigDecimal sumQtyRes=announceDAO.checkQtyReservations(announce.getId(),false);
