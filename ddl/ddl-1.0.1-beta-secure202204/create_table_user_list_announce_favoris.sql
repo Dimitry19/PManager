1eb1f9ea564672946bf6aabd651b69f79a840728
DNdjouomo <dimitri.sime.ndjouomo@grupposinapsi.it>
2022-06-08 18:33:02 +0200

    Merging branches

---
+++ ddl/ddl-1.0.1-beta-secure202204/create_table_user_list_announce_favoris.sql
@@ -0,0 +1,11 @@
+-- Recuperer le script de creation de la table H2 ou MySql
+create table user_list_announce_favoris
+(
+    USER_ID     BIGINT not null,
+    ANNOUNCE_ID BIGINT not null,
+    primary key (USER_ID, ANNOUNCE_ID),
+    constraint FK1Y6CLURE4R2YJMN2TX8ERSXR8
+        foreign key (USER_ID) references TP_USER (ID),
+    constraint FKF3UV1T15EIKMGVASQJMQ4AXLC
+        foreign key (ANNOUNCE_ID) references ANNOUNCE (ID)
+);
